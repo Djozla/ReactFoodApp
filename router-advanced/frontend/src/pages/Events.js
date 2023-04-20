f832cd804b2b2d6a45acfa0cd443caf262b9ac52
Djozla <djordje.djuric.20@singimail.rs>
2023-04-20 22:19:32 +0200

    Dopunjavanje Eventova razradjivanje ruta i citanje iz lokalne baze

--- router-advanced/frontend/src/pages/Events.js
+++ router-advanced/frontend/src/pages/Events.js
@@ -1,5 +1,26 @@
+import { useLoaderData } from "react-router-dom";
+import { json } from "react-router-dom";
+
+import EventsList from "../components/EventsList";
+
 function EventsPage() {
-  return <h1>Events Page</h1>;
+  const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
+
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+  if (!response.ok) {
+    throw json({ message: "Could not save event" }, { status: 500 });
+  } else {
+    return response;
+  }
+}
